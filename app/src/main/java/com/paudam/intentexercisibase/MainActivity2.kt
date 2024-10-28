6d78a6da55ed2fb55e51dec2199476fe2f9b7646
paupc <p.pulido.c@institutvidreres.cat>
2024-10-28 12:27:00 +0100

    Pasa valores de uno a otro con binding

--- app/src/main/java/com/paudam/intentexercisibase/MainActivity2.kt
+++ app/src/main/java/com/paudam/intentexercisibase/MainActivity2.kt
@@ -1,38 +1,39 @@
 package com.paudam.intentexercisibase
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 import androidx.databinding.DataBindingUtil
+import com.paudam.intentexercisibase.databinding.ActivityMain2Binding
 import com.paudam.intentexercisibase.databinding.ActivityMainBinding
 
+class MainActivity2 : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
+    lateinit var v_nom:String
+    lateinit var v_dni:String
+
     val KEY_NOM="nom"
     val KEY_DNI="dni"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMain2Binding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
         enableEdgeToEdge()
+       // setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding.sendButton.setOnClickListener {
+        //con esto haces un get intent, con esto obtienes los intents de otras variables
+    val intent2 = intent
+        binding.textViewNom.text = intent2.getStringExtra(KEY_NOM)
+        binding.textViewDNI.text = intent2.getStringExtra(KEY_DNI)
 
-            val intent = Intent(applicationContext, MainActivity2::class.java)
-            intent.putExtra(KEY_NOM, binding.nomText.text.toString())
-            intent.putExtra(KEY_DNI,  binding.dniText.text.toString())
-            startActivity(intent)
 
-        }
 
 
 
